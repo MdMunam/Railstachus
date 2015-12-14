d1a27f2fb39acd06cfabb29984b2aa2fbdc4801b
Md Munam <mdmunam@infotachus.com>
2015-12-14 17:04:36 +0530

    First Commit

---
+++ app/models/user.rb
@@ -0,0 +1,9 @@
+class User < ActiveRecord::Base
+  # Include default devise modules. Others available are:
+  # :confirmable, :lockable, :timeoutable and :omniauthable
+  devise :database_authenticatable, :confirmable, :registerable,
+         :recoverable, :rememberable, :trackable, :validatable
+
+  # validates_presence_of :phone_number
+  # validates :phone_number, numericality: true
+end
